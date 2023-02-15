fb012a376e84fd8aed2d32c6e50edc23fd36577a
JensJansen <jens2798@gmail.com>
2023-02-15 14:30:44 -0500

    Other files I meant to commit

---
+++ DPSCalc/boostConfiguration.py
@@ -0,0 +1,15 @@
+
+
+class BoostConfiguration():
+    def __init__self(self, attackPot: int, strengthPot: int, defPot: int, rangedPot: int, magePot: int, attackPrayer: float, strPrayer: float, defPrayer: float, rangedPrayer: float, magePrayer: float):
+        self.attackPot = attackPot
+        self.strengthPot = strengthPot
+        self.defPot = defPot
+        self.magePot = magePot
+        self.rangedPot = rangedPot
+        self.attackPrayer = attackPrayer
+        self.strengthPrayer = strPrayer
+        self.defPrayer = defPrayer
+        self.rangedPrayer = rangedPrayer
+        self.magePrayer = magePrayer
+    
