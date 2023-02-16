d967127a253794569a8a888a858c860e0b4008b5
Josh Elliott <josh@joshelliott.me>
2023-02-16 13:02:05 -0500

    Adding first pass at the monster class

--- vault/monster.py
+++ vault/monster.py
@@ -1,3 +1,29 @@
+DEFAULT_LEVEL_VALUE = 1
+DEFAULT_STAT_VALUE = 0
+DEFAULT_ATTACK_SPEED = 4
+
 class Monster():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, data) -> None:
+        for key, value in data.items():
+            setattr(self, key, value)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __eq__(self, other) -> bool:
+        return self.name == other.name
+
+    def getSkillLevel(self, skill) -> int:
+        return DEFAULT_LEVEL_VALUE if not hasattr(self, "skills") or skill.lower() not in self.skills.keys() else self.skills[skill.lower()]
+
+    def getBonus(self, stat) -> int:
+        return DEFAULT_STAT_VALUE if not hasattr(self, "bonuses") or stat.lower() not in self.bonuses.keys() else self.bonuses[stat.lower()]
+
+    def getAttackStyles(self) -> list:
+        return self.attack_styles if hasattr(self, "attack_styles") else []
+
+    def getAttackSpeed(self) -> int:
+        return self.attack_speed if hasattr(self, "attack_speed") else []
+
+    def getAttributes(self) -> list:
+        return self.attributes if hasattr(self, "attributes") else []
--- vault/vault.py
+++ vault/vault.py
@@ -5,9 +5,10 @@ from monster import Monster
 from player import Player
 from gear_set import GearSet
 
-from constants import GEAR_STATS, GEAR_SLOTS
+from constants import GEAR_STATS, GEAR_SLOTS, SKILL_TYPES, ENEMY_STATS
 
 ITEM_DIR = "/items"
+MONSTER_DIR = "/monsters"
 
 class Vault():
     def __init__(self, dirPath) -> None:
@@ -39,6 +40,17 @@ class Vault():
             item_data = json.load(file)
             return Item(item_data)
 
+    def getMonsterByName(self, name) -> Monster:
+        filename = f"{name.replace(' ', '_').lower()}.json"
+        filepath = f"{self.dirPath}{MONSTER_DIR}/{filename}"
+        if not os.path.isfile(filepath):
+            print(f"[ERROR] Unable to find file {filepath}")
+            return None
+
+        with open(filepath, 'r') as file:
+            item_data = json.load(file)
+            return Monster(item_data)
+
 if __name__ == "__main__":
     vault = Vault("./data")
     test_item = vault.getItemByName("Bow of Faerdhinen")
@@ -47,7 +59,7 @@ if __name__ == "__main__":
     #for stat in GEAR_STATS:
     #    print(stat + ": " + str(test_item.getBonus(stat)))
 
-    test_gear = GearSet()
+    '''test_gear = GearSet()
     print(test_gear)
     print(test_gear.getAttackSpeed())
     print(test_gear.attack_type)
@@ -56,4 +68,11 @@ if __name__ == "__main__":
     print(test_gear.getBonus(GEAR_STATS.ATTACK_RANGE))
     print(test_gear.getAttackSpeed())
     print(test_gear.attack_type)
-    print(test_gear.getAttackBonus())
+    print(test_gear.getAttackBonus())'''
+
+    test_monster = vault.getMonsterByName("King Black Dragon")
+    print(test_monster)
+    print(test_monster.getAttackSpeed())
+    print(test_monster.getAttributes())
+    print(test_monster.getSkillLevel(SKILL_TYPES.HITPOINTS))
+    print(test_monster.getBonus(ENEMY_STATS.RANGE_STRENGTH))
